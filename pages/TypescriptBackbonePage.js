b86383bf8fe887bf741d7f46466db20c95d7458e
Shreya Biswas <35816503+bshreya74@users.noreply.github.com>
2021-06-13 20:14:31 -0400

    Added TypescriptAngularPage.js and TypescriptBackbonePage.js

---
+++ pages/TypescriptBackbonePage.js
@@ -0,0 +1,99 @@
+module.exports ={
+    url : 'https://todomvc.com/examples/typescript-backbone/',
+    
+    elements: {
+        header: '.header',
+        todoplaceholder : 'input[class = "new-todo"]',
+    },
+
+    commands : [{
+        pressEnter(browser){
+            return browser
+                        .keys(browser.Keys.ENTER);
+        },
+
+        completeTodo(browser, todo){
+            return browser
+                .elements('css selector', 'ul[class = "todo-list"] div', function(elementList){
+                    elementList.value.forEach(function (elementsObj, index) {
+                        browser.elementIdText(elementsObj.ELEMENT, function (result) {
+                            if(result.value == todo){
+                                index = index+1
+                                browser.useXpath().click("/html/body/section/section/ul/li["+index+"]/div/input")
+                            }
+                        });
+                    });
+                });
+        },
+
+        uncheckTodo(browser, todo){
+            return browser
+                .elements('css selector', 'li[class = "completed"] div', function(elementChecked){
+                    elementChecked.value.forEach(function (elementsObject, index) {
+                        browser.elementIdText(elementsObject.ELEMENT, function (res) {
+                            if(res.value == todo){
+                                index = index+1
+                                browser.useXpath().click("/html/body/section/section/ul/li["+index+"]/div/input")
+                            }
+                        });
+                    });
+                });
+        },
+
+        editTodo(browser, todo, newValue){
+            return browser
+                .elements('css selector', 'ul[class = "todo-list"] div', function(elementList){
+                    elementList.value.forEach(function (elementsObj, index) {
+                        browser.elementIdText(elementsObj.ELEMENT, function (result) {
+                            if(result.value == todo){
+                                browser
+                                    .moveTo(elementsObj.ELEMENT)
+                                    .pause(100)
+                                    .doubleClick()
+                                    .pause(100)
+                                    .keys(newValue)
+                                    .keys(browser.Keys.ENTER)
+                                    .pause(100)
+                            }
+                        });
+                    });
+                });
+        },
+
+        removeTodo(browser, todo){
+            return browser
+                .elements('css selector', 'ul[class = "todo-list"] div', function(elementList){
+                    elementList.value.forEach(function (elementsObj, index) {
+                        browser.elementIdText(elementsObj.ELEMENT, function (result) {
+                            if(result.value == todo){
+                                index = index+1
+                                browser
+                                    .moveTo(elementsObj.ELEMENT)
+                                    .useXpath()
+                                    //.waitForElementVisible("/html/body/section/section/ul/li["+index+"]/div/button")
+                                    .click("/html/body/section/section/ul/li["+index+"]/div/button");
+                            }
+                        });
+                    });
+                })
+        },
+
+        viewActiveTodos(browser){
+            return browser
+                .elements('css selector', 'div[class = "view display"] label', function(elementListNew){
+                    console.log("Remaining todos:")
+                    elementListNew.value.forEach(function (elementsObject) {
+                        browser.elementIdText(elementsObject.ELEMENT, function (result) {
+                            console.log(result.value)
+                        })
+                    })
+                });
+        },
+
+        clearCompleted(browser){
+            return browser
+                .useCss().click("button[class = 'clear-completed'")
+        }
+
+    }]
+}
